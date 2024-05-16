ab3365efe8329d6f85e38b20524e43def62756d5
ASL11 <ashish.samal@3ds.com>
2024-05-16 18:46:40 +0530

    Added content to file VariablesAndDataTypeSession.java

--- src/topic1_VaiablesAndDataTypes/VariablesAndDataTypeSession.java
+++ src/topic1_VaiablesAndDataTypes/VariablesAndDataTypeSession.java
@@ -1,4 +1,26 @@
 package topic1_VaiablesAndDataTypes;
 
 public class VariablesAndDataTypeSession {
+    public static void main(String[] args) {
+        //Arithmetic operators
+        // (Binary) */%+-
+        //unary
+
+
+        //relational operators  <,>,>=,<=,==,!=
+//        int a = 12;
+//        int b = 10;
+//        System.out.println(a>b);
+
+        //logical operators
+        //&&, ||, !
+
+        System.out.println(!(3 > 2));
+
+        //Assignment operators
+        //=, +=, -=, /=, *=
+        int a = 3;
+        int c = a *= 2;
+        System.out.println(c);
+    }
 }
